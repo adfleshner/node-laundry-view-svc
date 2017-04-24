5951b1fe19bf9de4930ce32a4bf3d5beaf6ed554
CAPS\afleshner <aaron.fleshner@ua.edu>
2017-04-24 14:35:43 -0500

    Removing unneeded campuses
    Removed the original Campuses from the campus config and added comment
    Load UA

--- index.js
+++ index.js
@@ -7,14 +7,7 @@ var app = express();
 
 var roomCache = {},
     campusConfigs = {
-      // 216: {
-      //   id:'pcv',
-      //   name:'Peter Cooper Village'
-      // },
-      // 219: {
-      //   id:'st',
-      //   name:'Stuyvesant Town'
-      // }
+      //Load the university of Alabama
       27:{
         id:'ua',
         name:'University of Alabama'
